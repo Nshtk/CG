2cce0e177a12f6fd77f36d5efa24b0f1576efbcb
Nshtk <slava2034@gmail.com>
2021-12-11 18:08:29 +0300

    CG3 and CG4

---
+++ CompGraph3/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompGraph3
+{
+    public partial class Form1 : Form
+    {
+        private List<string> custom_data = new List<string>();
+        private GraphicsPath[] graphics_paths = new GraphicsPath[2];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
+            PreviewKeyDown += new PreviewKeyDownEventHandler(Form1_PreviewKeyDown);
+            Shown += new EventHandler(Form1_Shown);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            toolTip1.SetToolTip(textBox1, "Перечислите дополнительные данные через запятую. Данные используются в соответствии с порядком ключевых слов в текущем действии.");
+
+            int half_height = pictureBox1.Height / 2, half_width = pictureBox1.Width / 2;
+            int size_x_1 = 300, size_x_2 = 200, size_y_1 = 100, size_y_2 = 200;
+            int x_1 = half_width - size_x_1 / 2, y_1 = half_height - size_y_1 / 2; 
+            int x_2 = half_width - size_x_2 / 2, y_2 = half_height - size_y_2 / 2;
+
+            for (int i = 0; i < graphics_paths.Length; i++)
+                graphics_paths[i] = new GraphicsPath();
+
+            graphics_paths[0].AddEllipse(x_1, y_1, size_x_1, size_y_1);
+            graphics_paths[1].AddEllipse(x_2, y_2, size_x_2, size_y_2);
+        }
+        private void Form1_Shown(Object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+        private bool getCustomData(uint number_of_parameters)
+        {
+            string str = textBox1.Text, buf = "";
+
+            int length_minus_one = str.Length - 1;
+            custom_data.Clear();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((Char.IsDigit(str[i]) || str[i] == '.') && str[i] != ' ' && str[i] != ',' && str[i] != '-')
+                    buf += str[i];
+                else if(str[i] != '.' && str[i] != ' ' && str[i] != ',' && str[i] != '-')
+                {
+                    MessageBox.Show("Данные введены в неверном формате.");
+                    return false;
+                }
+                if (buf.Length != 0 && (str[i] == ',' || i == length_minus_one))
+                    custom_data.Add(buf);
+            }
+
+            if (custom_data.Count != number_of_parameters)
+            {
+                MessageBox.Show("Данные не введены или введены неверно.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+            }
+        }
+        public void Form1_AxisShiftKeyDown(object sender, KeyEventArgs e)
+        {
+            int offset = 20;
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(1, 0, 0, 0, 1, 0, 0, -offset, 1);
+                    break;
+                case Keys.Down:
+                    transformShape(1, 0, 0, 0, 1, 0, 0, offset, 1);
+                    break;
+                case Keys.Left:
+                    transformShape(1, 0, 0, 0, 1, 0, -offset, 0, 1);
+                    break;
+                case Keys.Right:
+                    transformShape(1, 0, 0, 0, 1, 0, offset, 0, 1);
+                    break;
+            }
+        }
+        public void Form1_AxisMirroringKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(1, 0, 0, 0, -1, 0, 0, 400, 1);
+                    break;
+                case Keys.Down:
+                    transformShape(1, 0, 0, 0, -1, 0, 0, 700, 1);
+                    break;
+                case Keys.Left:
+                    transformShape(-1, 0, 0, 0, 1, 0, 500, 0, 1);
+                    break;
+                case Keys.Right:
+                    transformShape(-1, 0, 0, 0, 1, 0, 1000, 0, 1);
+                    break;
+            }
+        }
+        public void Form1_EquationMirroringKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(0, 1, 0, 1, 0, 0, 0, 0, 1);
+                    break;
+            }
+        }
+        public void Form1_ScalingKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(1, 0, 0, 0, 1.4, 0, 0, -110, 1);
+                    break;
+                case Keys.Down:
+                    transformShape(1, 0, 0, 0, 0.4, 0, 0, 160, 1);
+                    break;
+                case Keys.Left:
+                    transformShape(0.4, 0, 0, 0, 1, 0, 210, 0, 1);
+                    break;
+                case Keys.Right:
+                    transformShape(1.4, 0, 0, 0, 1, 0, -140, 0, 1);
+                    break;
+            }
+        }
+        public void Form1_CenterRotationKeyDown(object sender, KeyEventArgs e)
+        {
+            double angle, n, m;
+            Double.TryParse(custom_data[0], out angle);
+
+            angle = angle * Math.PI / 180;
+            m = 371;
+            n = 270;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(Math.Cos(angle), Math.Sin(angle), 0, -Math.Sin(angle), Math.Cos(angle), 0, -m*(Math.Cos(angle)-1)+n* Math.Sin(angle), -n * (Math.Cos(angle) - 1) - m * Math.Sin(angle), 0.01);
+                    break;
+                case Keys.Down:
+                    transformShape(Math.Cos(-angle), Math.Sin(-angle), 0, -Math.Sin(-angle), Math.Cos(-angle), 0, -m * (Math.Cos(-angle) - 1) + n * Math.Sin(-angle), -n * (Math.Cos(-angle) - 1) - m * Math.Sin(-angle), 0.01);
+                    break;
+            }
+        }
+        public void Form1_CustomRotationKeyDown(object sender, KeyEventArgs e)
+        {
+            double angle, n, m;
+            Double.TryParse(custom_data[0], out angle);
+            Double.TryParse(custom_data[1], out m);
+            Double.TryParse(custom_data[2], out n);
+
+            angle = angle * Math.PI / 180;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    transformShape(Math.Cos(angle), Math.Sin(angle), 0, -Math.Sin(angle), Math.Cos(angle), 0, -m * (Math.Cos(angle) - 1) + n * Math.Sin(angle), -n * (Math.Cos(angle) - 1) - m * Math.Sin(angle), 0.01);
+                    break;
+                case Keys.Down:
+                    transformShape(Math.Cos(-angle), Math.Sin(-angle), 0, -Math.Sin(-angle), Math.Cos(-angle), 0, -m * (Math.Cos(-angle) - 1) + n * Math.Sin(-angle), -n * (Math.Cos(-angle) - 1) - m * Math.Sin(-angle), 0.01);
+                    break;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            int half_height = pictureBox1.Height / 2, half_width = pictureBox1.Width / 2;
+            Graphics gr = e.Graphics;
+
+            gr.Clear(Color.White);
+
+            gr.DrawLine(Pens.Black, half_width, 0, half_width, pictureBox1.Height);
+            gr.DrawLine(Pens.Black, 0, half_height, pictureBox1.Width, half_height);
+
+            for (int i = 0; i < graphics_paths.Length; i++)
+                gr.DrawPath(Pens.Black, graphics_paths[i]);
+
+            Region reg = new Region(graphics_paths[0]);
+            for (int i = 1; i < graphics_paths.Length; i++)
+                reg.Intersect(graphics_paths[i]);
+            gr.FillRegion(Brushes.Gray, reg);
+        }
+        private void matrixMultiply(ref PointF[] points, in double[,] transform_matrix)
+        {
+            double[,] result = new double[points.Length, 3];
+            for (int i = 0; i < points.Length; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i, j] += points[i].X * transform_matrix[0, j];
+                    result[i, j] += points[i].Y * transform_matrix[1, j];
+                    result[i, j] += 1 * transform_matrix[2, j];
+                }
+                points[i].X = (float)result[i, 0];
+                points[i].Y = (float)result[i, 1];
+            }
+        }
+        private void transformShape(double a, double b, int p, double c, double d, double q, double m, double n, double s)
+        {
+            double[,] transform_matrix = new double[3, 3]
+            {
+                {a,b,p},
+                {c,d,q},
+                {m,n,s}
+            };
+
+            PointF[] points;
+            for (int i=0; i<graphics_paths.Length; i++)
+            {
+                points = graphics_paths[i].PathPoints;
+                matrixMultiply(ref points, in transform_matrix);
+                graphics_paths[i] = new GraphicsPath(points, graphics_paths[i].PathTypes);
+            }
+
+            pictureBox1.Invalidate();
+        }
+        private void restoreDefaults()
+        {
+            textBox1.Text = "";
+            label4.Text = "—";
+
+            int half_height = pictureBox1.Height / 2, half_width = pictureBox1.Width / 2;
+            int size_x_1 = 300, size_x_2 = 200, size_y_1 = 100, size_y_2 = 200;
+            int x_1 = half_width - size_x_1 / 2, y_1 = half_height - size_y_1 / 2;
+            int x_2 = half_width - size_x_2 / 2, y_2 = half_height - size_y_2 / 2;
+
+            for (int i = 0; i < graphics_paths.Length; i++)
+                graphics_paths[i].Reset();
+
+            graphics_paths[0].AddEllipse(x_1, y_1, size_x_1, size_y_1);
+            graphics_paths[1].AddEllipse(x_2, y_2, size_x_2, size_y_2);
+
+            pictureBox1.Invalidate();
+        }
+        private void unsubscribeFromKeyEvent()
+        {
+            KeyDown -= Form1_AxisShiftKeyDown;
+            KeyDown -= Form1_AxisMirroringKeyDown;
+            KeyDown -= Form1_EquationMirroringKeyDown;
+            KeyDown -= Form1_ScalingKeyDown;
+            KeyDown -= Form1_CenterRotationKeyDown;
+            KeyDown -= Form1_CustomRotationKeyDown;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            unsubscribeFromKeyEvent();
+            KeyPreview = true;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    label4.Text = "Стрелки: вверх, вниз, влево,\nвправо.";
+                    KeyDown += Form1_AxisShiftKeyDown;
+                    break;
+                case 1:
+                    label4.Text = "Стрелки: вверх, вниз, влево,\nвправо.";
+                    KeyDown += Form1_AxisMirroringKeyDown;
+                    break;
+                case 2:
+                    label4.Text = "Стрелки: вверх.";
+                    KeyDown += Form1_EquationMirroringKeyDown;
+                    break;
+                case 3:
+                    label4.Text = "Стрелки: вверх, вниз, влево,\nвправо.";
+                    KeyDown += Form1_ScalingKeyDown;
+                    break;
+                case 6:
+                    restoreDefaults();
+                    break;
+                default:
+                    MessageBox.Show("Введите дополнительные данные и нажмите \"Принять\".");
+                    label4.Text = "—";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 4:
+                    if (getCustomData(1))
+                    {
+                        label4.Text = "Стрелки: вверх, вниз.";
+                        KeyDown += Form1_CenterRotationKeyDown;
+                    }
+                    else
+                        KeyDown -= Form1_CenterRotationKeyDown;
+                    break;
+                case 5:
+                    if (getCustomData(3))
+                    {
+                        label4.Text = "Стрелки: вверх, вниз.";
+                        KeyDown += Form1_CustomRotationKeyDown;
+                    }
+                    else
+                        KeyDown -= Form1_CustomRotationKeyDown;
+                    break;
+            }
+        }
+    }
+}
---
+++ CompGraph4/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace CompGraph4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(238, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите преобразование";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 210;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.DropDownWidth = 423;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Перенос по осям",
+            "Отражение относительно осей",
+            "Отражение относительно прямой Y=X",
+            "Масштабирование",
+            "Поворот на заданный угол относительно центра координат",
+            "Поворот на заданный угол относительно произвольной точки",
+            "Восстановить вид по умолчанию"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 55);
+            this.comboBox1.MaxDropDownItems = 20;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Дополнительные данные";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 900000000;
+            this.toolTip1.InitialDelay = 1;
+            this.toolTip1.ReshowDelay = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 26);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Принять";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Управление:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "— ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 625);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
